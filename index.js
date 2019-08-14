aca1d86b1beb1c02fdf9756a03d97b1d36133d6f
djdagorne <dj.dagorne@gmail.com>
2019-08-14 15:45:14 -0400

    working on obtaining id from elements

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const artistSearchUrl = 'https://tastedive.com/api/similar';
-const artistApiKey = '342382-concertf-BEYCE2OB';
+const artistSearchUrl = 'http://ws.audioscrobbler.com/2.0/';
+const artistApiKey = 'cca25a181d5244cbe7e1d7b94d8ce6f4';
 
 const concertSearchUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
 const concertApiKey = 'hmha3Dr0BjItrX7K98A1aK2Cxn7Cg2nn';
@@ -10,48 +10,106 @@ function displayResults(){
 
 };
 
-function formatArtistQueryParams(artistParams) { //turning an object of key:value pairs into an array, then formatting it into a GET request
+function formatQueryParams(artistParams) { //turning an object of key:value pairs into an array, then formatting it into a GET request
     const queryItems = Object.keys(artistParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(artistParams[key])}`)
     return queryItems.join('&');
 }
 
 function getArtistList(searchArtist, searchCity){
     console.log('getting artist list');
-    const artistParams = { //necessary keys for tastedive API
-        q: searchArtist,
-        type: 'music',
-        k: artistApiKey
+    const artistParams = { //necessary keys for lastfm API
+        method: 'artist.getSimilar',
+        artist: searchArtist,
+        api_key: artistApiKey,
+        format: 'json',
+        limit: '10'
     };
 
-    const artistQueryString = formatArtistQueryParams(artistParams);
-    const artistUrl = artistSearchUrl + '?' + artistQueryString; //formatting the GET request
+    const artistQueryString = formatQueryParams(artistParams);
+    const artistUrl = artistSearchUrl + '?' + artistQueryString; 
 
-    console.log('artistUrl = ' + artistUrl);
-
-    fetch(artistUrl, {mode: 'cors'})
+    fetch(artistUrl)
         .then(response => {
-          console.log('first .then')
-          return response.json();
+            console.log('fetching lastFM API response');
+            return response.json();
         })
         .then(responseJson =>{
-            if(responseJson.similar.results.length>0){
+            console.log(responseJson);
+            if(responseJson.similarartists.artist.length>0){
                 $('#js-error-message').empty();
-                getEventList(responseJson);
+                displayArtistList(responseJson);
             }else{
                 $('#results').addClass('hidden');
                 $('#results-list').empty();
-                throw new Error('no results found');
+                throw new Error('No results found.');
             }
         })
         .catch(err => {
-          console.log('this url is throwing an error: ' + err)
+            console.log('this url is throwing an error: ' + err)
             $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
 };
 
-function getEventList(artistListJson){
+function displayArtistList(responseJson){
+    console.log('rendering artist list to the DOM based on responseJson')
+    $('#results-list').empty();
+    const artistList = responseJson.similarartists.artist; //im lazy
+    for (let i=0; i < artistList.length; i++){ ////////////////////////////////////////////////////////////////figure out a naming convention for my id tags
+        $('#results-list').append(
+            `<li>
+                <h3>${artistList[i].name}</h3>
+                <a id="js-concert-expand" href='#'>${artistList[i].name}'s Concerts</a>
+                <section id="concert-results" class="hidden">
+                    <h4>${artistList[i].name} concert results</h4>
+                    <p id="js-concert-error-message" class="error-message hidden"></p>
+                    <ul id="${artistList[i].name}-results-list">
+                    </ul>
+                </section>
+            </li>`
+    )};
+    $('#results').removeClass('hidden');
+}
 
+function watchConcertList(){
+    
+}
+
+function getEventList(artistResponseJson, searchCity){
+    
+
+    // for(i=0,i<artistListJson.similarArtists.artist.length,i++){
+
+    // };
+    const eventParams = {
+        classification: 'music',
+        apikey: concertApiKey,
+        keyword: 'elvis', //testing
+        //city: searchCity,
+        sort: 'date,asc',
+    }
+    const eventQueryString = formatQueryParams(eventParams);
+    const eventUrl = concertSearchUrl + '?' + eventQueryString; 
+    
+    fetch(eventUrl)
+        .then(response => {
+            console.log('fetching tmAPI response');
+            return response.json();
+        })
+        .then(responseJson => {
+            console.log('this is our tmAPI reponse');
+            console.log(responseJson);
+        })
+        .catch(err => {
+            console.log('this url is throwing an error: ' + err)
+            $('#js-concert-error-message').text(`Something went wrong: ${err.message}`);
+        });
 };
+
+function getItemIdFromElement(item) {
+    return $(item)
+      .closest('li')
+      .data('item-id');
+  }
 
 function watchForm(){
     $('form').submit(event => {
@@ -60,6 +118,15 @@ function watchForm(){
         const searchCity = $('#js-search-city').val();
         //console.log('form submitted, ' + searchArtist + " " + searchCity);
         getArtistList(searchArtist, searchCity);
+    });
+
+    $('li').on('click', '#js-concert-expend', event => {
+        event.preventDefault();
+        console.log('clicking');
+        const id = getItemIdFromElement(event.currentTarget);
+        toggleCheckedForListItem(id);
+        renderShoppingList();   
+        //getEventList();
     });
 };
 
